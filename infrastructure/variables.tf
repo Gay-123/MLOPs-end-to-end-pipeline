9494c518fb57f5145d657a89fdbc6100b3050620
Gayathri T <gayathrit726@gmail.com>
2025-10-23 21:40:34 +0530

    Updated file

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -14,7 +14,7 @@ variable "location" {
 variable "acr_name" {
   description = "Name of Azure Container Registry"
   type        = string
-  default     = "mlopsacr-gayathri01"
+  default     = "mlopsacrgayathripipelineproject"
 }
 
 variable "aks_name" {
