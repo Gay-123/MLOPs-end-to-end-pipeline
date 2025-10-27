ff005d5f220db1759fec6ca255e3e3ea224b36e8
Gayathri T <gayathrit726@gmail.com>
2025-10-27 13:23:41 +0530

    Updated main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -36,9 +36,12 @@ resource "azurerm_kubernetes_cluster" "aks" {
     type = "SystemAssigned"
   }
 
-  acr_profile {
-    acr_id = azurerm_container_registry.acr.id
-  }
-
   depends_on = [azurerm_container_registry.acr]
+}
+
+
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
 }
